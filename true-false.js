954214300023a55e9e56c0f0aa2675f873216c86
ShuvoDas1 <67542645+ShuvoDas1@users.noreply.github.com>
2020-08-07 01:38:31 +0600

    true-fals

---
+++ true-false.js
@@ -0,0 +1,22 @@
+//Truthy
+//'0'
+// ' '
+//[]
+//{}
+//true
+
+//Falsy
+//0
+//''
+//null
+//undefined
+//NaN
+//false
+
+let number =true;
+if(number){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
