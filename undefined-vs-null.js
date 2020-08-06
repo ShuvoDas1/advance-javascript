bcd61ce0015f2510c406f492cbee17d56de2f0ee
ShuvoDas1 <67542645+ShuvoDas1@users.noreply.github.com>
2020-08-07 02:13:20 +0600

    undefined vs null concept for interview

---
+++ undefined-vs-null.js
@@ -0,0 +1,26 @@
+//Undefined
+let name; // not define value
+
+function add(num1,num2){
+    console.log(num1+num2);
+    //not define return
+}
+
+function add(num1,num2){
+   console.log(num1,num2);
+    return  //not defined return value
+}
+
+function add(num1,num2){
+    console.log(num1,num2)
+}
+let result = add(10);//not pass  parameter value
+console.log(result);
+
+const person = {name: 'shuvo', home: 'chittagong'};
+console.log(person.phone); // Not define this property
+
+let fun = undefined;
+console.log(fun);
+
+//null(not exiting)
